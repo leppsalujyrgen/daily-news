bf07703a0e6f097f660f6bc353b751c6b57b5b91
leppsalujyrgen <leppsalujyrgen@gmail.com>
2019-04-01 21:30:47 +0300

    Updates

---
+++ precache-manifest.8aaa5d9ed265aea89f06e7d9dd5c497f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ac268d18e3b84fd1cbc",
+    "url": "/daily.news/static/js/runtime~main.bbe41ea8.js"
+  },
+  {
+    "revision": "470dad618c2da5eaa77b",
+    "url": "/daily.news/static/js/main.6d067d8b.chunk.js"
+  },
+  {
+    "revision": "4d7734ec9fbba6ea70cd",
+    "url": "/daily.news/static/js/2.de87e1e9.chunk.js"
+  },
+  {
+    "revision": "470dad618c2da5eaa77b",
+    "url": "/daily.news/static/css/main.3a867b1c.chunk.css"
+  },
+  {
+    "revision": "e5d26cd92c4669bf7c710147bf9f92b7",
+    "url": "/daily.news/index.html"
+  }
+];
