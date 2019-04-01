5f8d2e8804cc66c831cedc137f3f29428f52461b
leppsalujyrgen <leppsalujyrgen@gmail.com>
2019-04-01 21:13:46 +0300

    Daily mail. Web Application.

---
+++ src/components/ArticleFeed.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import client from '../apollo';
+import gql from 'graphql-tag';
+import Article from './Article';
+
+
+const query =  gql`
+query{
+    articles {
+      title
+      source{
+        name
+      }
+      urlToImage
+      url
+    }
+}`
+
+const ArticleFeed = () => {
+
+    const [data, setData] = useState([])
+
+    client.query({query})
+    .then(response => setData(response.data.articles))
+    .catch(error => console.log(error))
+
+    return(
+        <div className="feed">
+            { data.map((article, indeks) => (
+            <Article 
+                isSmall={(indeks + 1) % 5 == 0 || (indeks + 2) % 5 == 0 || (indeks + 3) % 5 == 0} 
+                data={article} 
+                key={indeks}
+            />))}        
+        </div>
+    )
+}
+
+export default ArticleFeed;
